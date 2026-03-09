05dbcef5d66425136b5be0c3a28eee697207b392
GitHub Action <action@github.com>
2026-03-09 14:27:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/nonamecoders/tasklens/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/nonamecoders/tasklens/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.nonamecoders.tasklens.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
